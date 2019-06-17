d32bf64fde512c6445b05197d06200de2d2064cb
thangnch <43836831+thangnch@users.noreply.github.com>
2019-06-17 15:19:48 +0700

    Add files via upload

---
+++ SelfDriving.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Lambda, Conv2D, Dropout, Dense, Flatten
+from keras.regularizers import l2
+
+from utils import INPUT_SHAPE, batch_generator
+
+# Thư mục để dữ liệu
+data_dir = 'autocar'
+# Đọc file driving_log.csv với các cột tương ứng
+data_df = pd.read_csv(os.path.join(os.getcwd(), data_dir, 'driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
+
+# Lấy đường dẫn đến ảnh ở camera giữa, trái, phải
+X = data_df[['center', 'left', 'right']].values
+# Lấy góc lái của ô tô
+y = data_df['steering'].values
+
+# Vẽ histogram dữ liệu
+plt.hist(y)
+
+# Loại bỏ và chỉ lấy 1000 dữ liệu có góc lái ở 0
+pos_zero = np.array(np.where(y==0)).reshape(-1, 1)
+pos_none_zero = np.array(np.where(y!=0)).reshape(-1, 1)
+np.random.shuffle(pos_zero)
+pos_zero = pos_zero[:1000]
+
+pos_combined = np.vstack((pos_zero, pos_none_zero))
+pos_combined = list(pos_combined)
+
+y = y[pos_combined].reshape(len(pos_combined))
+X = X[pos_combined, :].reshape((len(pos_combined), 3))
+
+# After process
+plt.hist(y)
+
+# Chia ra traing set và validation set
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Xây dựng model
+model = Sequential()
+model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
+model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(64, 3, 3, activation='elu'))
+model.add(Conv2D(64, 3, 3, activation='elu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(100, activation='elu'))
+model.add(Dropout(0.5))
+model.add(Dense(50, activation='elu'))
+model.add(Dense(10, activation='elu'))
+model.add(Dense(1))
+model.summary()
+
+
+nb_epoch = 10
+samples_per_epoch = 1000
+batch_size = 32
+save_best_only = True
+learning_rate = 1e-4
+
+# Checkpoint này để nói cho model lưu lại model nếu validation loss thấp nhất
+checkpoint = ModelCheckpoint('autocar/model-{epoch:03d}.h5',
+                                 monitor='val_loss',
+                                 verbose=0,
+                                 save_best_only=save_best_only,
+                                 mode='auto')
+
+# Dùng mean_squrared_error làm loss function
+model.compile(loss='mean_squared_error', optimizer=Adam(lr=learning_rate))
+
+# Train model
+H = model.fit_generator(batch_generator(data_dir, X_train, y_train, batch_size, True),
+                    steps_per_epoch = samples_per_epoch,
+                    epochs = nb_epoch,
+                    max_q_size=1,
+                    validation_data=batch_generator(data_dir, X_valid, y_valid, batch_size, False),
+                    nb_val_samples=len(X_valid),
+                    callbacks=[checkpoint],
+                    verbose=1)
+
